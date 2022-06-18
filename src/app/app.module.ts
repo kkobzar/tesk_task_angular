75d8d4b0fe8f4204b138ca650a2aff184499037f
Kiril Kobzar <kirilkobzar@gmail.com>
2022-06-18 12:07:20 +0300

    timer displayer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TimeDisplayComponent } from './components/time-display/time-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeDisplayComponent
   ],
   imports: [
     BrowserModule
